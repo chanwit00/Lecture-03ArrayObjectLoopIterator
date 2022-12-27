d936ccf2970e48967d8c46776405add470501673
chanwit00 <120072936+chanwit00@users.noreply.github.com>
2022-12-27 21:11:34 +0700

    Create jsonDemo.js

---
+++ jsonDemo.js
@@ -0,0 +1,24 @@
+/****************************************************
+ * JavaScript for Beginers
+ * JS101 - JSON DEMO
+ * 
+ * BASICS : JSON Format
+ * STRINGIFY : JavaScript to JSON
+ * PARSE : JSON To JavaScript
+ */
+
+// Think of a simple JavaScript object with daya (property values)
+const book = new Object({ title : "1984" , author : "George Orwell"});
+console.log("\n --------- ")
+console.log(typeof book);
+console.log (book);
+/*
+/ you cna also have a collection of these objects
+
+const my Books = [
+    new Object ({title : "1984" , author : "George Orwell"}) ,
+    new Object ({title : "Becoming" , author : "Michelle Obama"}) ,
+    new Object ({title : "Snow Crash" , author : "Neal Stephenson"}) ,
+    new Object ({title : "Predictably Irrationcal" , author : "Dan Ariely"}) ,
+    
+*/
